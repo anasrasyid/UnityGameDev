f3f8986530b1149285ce1d9469697a49ab967af1
Anas Rasyid <47500014+anasrasyid@users.noreply.github.com>
2019-02-17 23:21:35 +0700

    Add files via upload

---
+++ Basic Scripting/Assets/Latihan1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class Latihan1 : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("Fungsi Awake terpanggil");
+    }
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Hello Dicoding!");
+    }
+    void FixedUpdate()
+    {
+        Debug.Log("Waktu untuk Fixed Update :" + Time.deltaTime);
+        Debug.Log("Saya Anas adalah game developer");
+    }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Waktu untuk Update :" + Time.deltaTime);
+    }
+    void LateUpdate()
+    {
+        Debug.Log("Waktu untuk Late Update :" + Time.deltaTime);
+    }
+}
+ 
---
+++ Basic Scripting/Assets/Latihan3.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Latihan3 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int c = Jumlah(40,60);
+		Debug.Log("Hasil Jumlah a dan b adalah :" + c);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	/// Fungsi untuk menjumlah 2 angka
+	int Jumlah(int a, int b)
+	{
+		return a + b;
+	}
+}
